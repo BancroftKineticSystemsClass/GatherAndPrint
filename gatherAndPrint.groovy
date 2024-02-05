d9bd5360dbd17e9f80b8aa907ed7629eeea0ee11
Kevin Harrington <kharrington@bancroftschool.org>
2024-02-05 08:32:16 -0500

    adding stubs

--- gatherAndPrint.groovy
+++ gatherAndPrint.groovy
@@ -1,1 +1,21 @@
 // code here
+
+GitHub getGithub() throws IOException {
+	File workspace = new File(System.getProperty("user.home") + "/bowler-workspace/");
+	if (!workspace.exists()) {
+		workspace.mkdir();
+	}
+	try {
+		PasswordManager.loadLoginData(workspace);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	//PasswordManager.login();
+	GitHub gh = PasswordManager.getGithub();
+	return gh;
+}
+
+GitHub github = getGithub();
+
+
